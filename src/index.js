7d16259559e6fbec4149bf8a42b72508e7eca89a
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2021-09-23 15:51:06 -0700

    Revert "Added transcripts track"
    This reverts commit 2ff363aaf5e4c9fdd04a7ede885ffa2695ec8fa4.

--- src/index.js
+++ src/index.js
@@ -1,37 +1,9 @@
 import register from 'higlass-register';
 
-import { BasicMultipleLineChart, StackedBarTrack } from 'higlass-multivec';
-import Arcs1DTrack from 'higlass-arcs';
-import { TranscriptsTrack } from 'higlass-transcripts';
-
 import PileupTrack from './PileupTrack';
 
 register({
   name: 'PileupTrack',
   track: PileupTrack,
   config: PileupTrack.config,
-});
-
-register({
-  name: 'BasicMultipleLineChart',
-  track: BasicMultipleLineChart,
-  config: BasicMultipleLineChart.config,
-});
-
-register({
-  name: 'StackedBarTrack',
-  track: StackedBarTrack,
-  config: StackedBarTrack.config,
-});
-
-register({
-  name: 'Arcs1DTrack',
-  track: Arcs1DTrack,
-  config: Arcs1DTrack.config,
-});
-
-register({
-  name: 'TranscriptsTrack',
-  track: TranscriptsTrack,
-  config: TranscriptsTrack.config,
 });
